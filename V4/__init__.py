94acbb96271e7eaeeea1d6933538c992bea61969
Mr Viincci Le Roy <60152334+MrViincciLeRoy@users.noreply.github.com>
2025-11-11 10:43:26 +0200

    Update __init__.py

--- V4/__init__.py
+++ V4/__init__.py
@@ -1,5 +1,19 @@
 """Research V4 package initialization"""
 
+import sys
+import os
+
+# Windows encoding fix - MUST BE FIRST
+if sys.platform == 'win32':
+    if hasattr(sys.stdout, 'reconfigure'):
+        try:
+            sys.stdout.reconfigure(encoding='utf-8')
+            sys.stderr.reconfigure(encoding='utf-8')
+        except:
+            pass
+    os.environ['PYTHONIOENCODING'] = 'utf-8'
+
+# Now do normal imports
 from .ConfigManager import ConfigManager
 from .FloraDatabase import FloraDatabase
 from .Spider import UniversalResearchSpider, research
